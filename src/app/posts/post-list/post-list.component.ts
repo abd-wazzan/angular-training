9bc2da24d1434414e01023ccea3d17a666987b55
Abdulrahman Wazzan <abdulrahmanwazzan.pro@gmail.com>
2023-03-14 12:25:57 +0400

    Delete post and link created post to the API response

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -14,12 +14,16 @@ export class PostListComponent implements OnInit, OnDestroy{
     constructor(public postsService: PostsService) {
     }
     ngOnInit() {
-        this.posts = this.postsService.getPosts();
+        this.postsService.getPosts();
         this.postsSub = this.postsService.getPostUpdatedListener()
             .subscribe((posts: Post[]) => {this.posts = posts});
     }
 
     ngOnDestroy() {
         this.postsSub.unsubscribe()
+    }
+
+    onDelete(id: string) {
+        this.postsService.deletePost(id);
     }
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,22 +1,59 @@
 import {Post} from "./post.model";
 import {Injectable} from "@angular/core";
-import {Subject} from "rxjs";
+import {map, Subject} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
     private posts: Post[] = [];
     private postsUpdated: Subject<Post[]> = new Subject<Post[]>();
 
+    constructor(private httpClient: HttpClient) {
+    }
+
     getPostUpdatedListener() {
         return this.postsUpdated.asObservable();
     }
-    getPosts(): Post[] {
-        return [...this.posts];
+
+    getPosts() {
+        this.httpClient.get<{ message: string, data: any }>('http://localhost:3000/api/posts')
+            .pipe(map((res) => {
+                return res.data.map(post => {
+                    return {
+                        id: post._id,
+                        title: post.title,
+                        content: post.content
+                    }
+                });
+            }))
+            .subscribe((transformedPosts) => {
+                this.posts = transformedPosts;
+                this.postsUpdated.next([...this.posts]);
+            });
     }
 
     addPost(title: string, content: string) {
-        const post: Post = {title: title, content: content};
-        this.posts.push(post);
-        this.postsUpdated.next([...this.posts]);
+        const post: Post = {id: null, title: title, content: content};
+        this.httpClient.post<{ message: string, data: any }>('http://localhost:3000/api/posts', post)
+            .pipe(map((res) => {
+                    return {
+                        id: res.data._id,
+                        title: res.data.title,
+                        content: res.data.content
+                    };
+                })
+            )
+            .subscribe((transformedPost) => {
+                this.posts.push(transformedPost);
+                this.postsUpdated.next([...this.posts]);
+            });
+    }
+
+    deletePost(id: string) {
+        this.httpClient.delete<{ message: string }>('http://localhost:3000/api/posts/' + id)
+            .subscribe((response) => {
+                this.posts = this.posts.filter(post => post.id != id);
+                this.postsUpdated.next([...this.posts]);
+            });
     }
 }
